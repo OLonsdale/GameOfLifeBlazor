4abbda00fa16833f6a399c2b500afaf2a5e686af
Oliver Lonsdale <oliver.lonsdale@fourquant.com>
2023-12-11 01:39:50 +0000

    Add project files.

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GameOfLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i != 50; i++)
+            {
+                for(int j = 0; j != 50; j++)
+                {
+                    Console.Write("■");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
